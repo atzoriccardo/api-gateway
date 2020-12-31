c8e04c182326c956e8a7c77e9b11a1f0071356a1
kreacherjs <riccardotzr@gmail.com>
2020-12-31 22:31:35 +0100

    Modify CI yaml and delete sh script

--- docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD" 
-docker build -t "$IMAGE_NAME":"$TRAVIS_TAG" .
-docker tag "$IMAGE_NAME":"$TRAVIS_TAG" "$DOCKER_HUB_IMAGE_NAME":"$TRAVIS_TAG"
-docker push "$DOCKER_HUB_IMAGE_NAME":"$TRAVIS_TAG"
