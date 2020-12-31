0cae5b390ed942e5640f6da99c18e253fd9b3641
kreacherjs <riccardotzr@gmail.com>
2020-12-31 21:39:24 +0100

    - Modifiy sh script

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD" 
+docker build -t "$IMAGE_NAME":"$TRAVIS_TAG" .
+docker tag "$IMAGE_NAME":"$TRAVIS_TAG" "$DOCKER_HUB_IMAGE_NAME":"$TRAVIS_TAG"
+docker push "$DOCKER_HUB_IMAGE_NAME":"$TRAVIS_TAG"
